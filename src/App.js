e28e82d157fca2ec8615361d9c6c5f6ec31f784f
ajhanson22227 <ajhanson22227@gmail.com>
2020-11-23 09:30:30 -0600

    fetchposts to a custom hook

--- src/App.js
+++ src/App.js
@@ -2,32 +2,12 @@ import React, { useEffect, useState } from 'react';
 import Post from './components/Post';
 
 const App = () => {
-	const [posts, setPosts] = useState(null);
-	const [postList, setPostList] = useState(null);
-
-	useEffect(() => {
-		async function fetchPosts() {
-			const response = await fetch(
-				`http://localhost:3000/api/posts/published`,
-			);
-			const data = await response.json();
-			setPosts(data);
-		}
-		fetchPosts();
-	}, []);
-
-	function displayPosts() {
-		console.log(`DISPLAYING WOOOO`);
-		let postArray = [];
-		for (let i in posts) {
-			postArray.push(<Post key={i} post={posts[i]} />);
-		}
-		setPostList(postArray);
-	}
+	const posts = usePosts();
+	const postList = usePostList(posts);
 
 	const handleClick = (e) => {
-		console.log(posts);
-		displayPosts();
+		// console.log(postList);
+		//displayPosts();
 	};
 
 	return (
@@ -38,5 +18,33 @@ const App = () => {
 		</div>
 	);
 };
+
+function usePosts() {
+	const [posts, setPosts] = useState(null);
+	useEffect(() => {
+		async function fetchPosts() {
+			const response = await fetch(
+				`http://localhost:3000/api/posts/published`,
+			);
+			const data = await response.json();
+			setPosts(data);
+		}
+		fetchPosts();
+	}, []);
+	return posts;
+}
+
+function usePostList(posts) {
+	const [postList, setPostList] = useState(null);
+
+	useEffect(() => {
+		let pArray = [];
+		for (let i in posts) {
+			pArray.push(<Post key={i} post={posts[i]} />);
+		}
+		setPostList(pArray);
+	}, [posts]);
+	return postList;
+}
 
 export default App;
