efa69e409e54a05da7b0367204651761cface893
ajhanson22227 <ajhanson22227@gmail.com>
2020-12-04 15:39:38 -0600

    Different NavList Whether signed in or not

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,20 +2,44 @@ import React from 'react';
 import './Navbar.css';
 import { Link } from 'react-router-dom';
 
-const Navbar = () => {
+const Navbar = ({ user }) => {
+	const signedOutNav = (
+		<ul>
+			<li>
+				<Link to='/'>Home</Link>
+			</li>
+			<li>
+				<Link to='/posts'>All Posts</Link>
+			</li>
+			<li>
+				<Link to='/user/register'>Sign Up</Link>
+			</li>
+			<li>
+				<Link to='/user/login'>Log In</Link>
+			</li>
+		</ul>
+	);
+
+	const loggedInNav = (
+		<ul>
+			<li>
+				<Link to='/'>Home</Link>
+			</li>
+			<li>
+				<Link to='/posts'>All Posts</Link>
+			</li>
+			<li>
+				<Link to='/user/logout'>Log Out</Link>
+			</li>
+		</ul>
+	);
+
 	return (
 		<div className='navbar'>
 			<div className='blog-title'>
 				<p>I Blog. You Read.</p>
 			</div>
-			<ul>
-				<li>
-					<Link to='/'>Home</Link>
-				</li>
-				<li>
-					<Link to='/posts'>All Posts</Link>
-				</li>
-			</ul>
+			{user == null ? signedOutNav : loggedInNav}
 		</div>
 	);
 };
