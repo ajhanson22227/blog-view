7ffbb2ab11db30d4ce866bd1206a043a0f67d7c6
ajhanson22227 <ajhanson22227@gmail.com>
2020-12-04 15:03:42 -0600

    Create user state variable, pass to other components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Homepage from './components/HomePage/Homepage';
 import PostPage from './components/PostPage/Postpage';
@@ -6,13 +6,19 @@ import PostsPage from './components/PostsPage/PostsPage';
 import Navbar from './components/Navbar/Navbar';
 
 const App = () => {
+	const [user, setUser] = useState(localStorage.getItem('user'));
+	console.log(user);
 	return (
 		<div>
-			<Navbar />
+			<Navbar user={user} setUser={setUser} />
 			<Switch>
-				<Route path='/' exact component={Homepage} />
-				<Route path='/post/:id' exact component={PostPage} />
-				<Route path='/posts' exact component={PostsPage} />
+				<Route path='/' exact component={() => <Homepage />} />
+				<Route
+					path='/post/:id'
+					exact
+					component={() => <PostPage user={user} />}
+				/>
+				<Route path='/posts' exact component={() => <PostsPage />} />
 			</Switch>
 		</div>
 	);
